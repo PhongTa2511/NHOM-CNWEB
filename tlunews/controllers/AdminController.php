e23031654cc16ee1f498a49daf6264c6085779e8
Ph11 <phongphu7015@gmail.com>
2024-12-03 15:56:51 +0700

    ''

---
+++ tlunews/controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+    class AdminController{
+        private $conn;
+        public function __construct($db){
+            $this->conn = $db;
+        }
+        public function add(){
+
+        }
+        public function edit(){}
+        public function delete(){}
+        public function index(){}
+
+    }
+
+?>
