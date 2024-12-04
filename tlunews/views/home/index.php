f1960b5e51ddc1e0a07b5f97a8aafe7595b2f283
NTLong2004 <xuddx1@gmail.com>
2024-12-04 20:51:07 +0700

    Update index.php

---
+++ tlunews/views/home/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Chủ</title>
+</head>
+<body>
+    <h1>Trang Chủ</h1>
+    <p>Chào mừng đến với trang tin tức!</p>
+    <a href="index.php?controller=news&action=index">Xem Tin Tức</a>
+</body>
+</html>
