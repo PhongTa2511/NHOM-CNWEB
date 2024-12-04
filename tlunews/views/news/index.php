aabffd52577fe8e72e331ebdb4bd669d6db43f4b
NTLong2004 <xuddx1@gmail.com>
2024-12-04 23:53:57 +0700

    Update index.php

--- tlunews/views/news/index.php
+++ tlunews/views/news/index.php
@@ -7,14 +7,20 @@
 </head>
 <body>
     <h1>Danh Sách Tin Tức</h1>
-    <ul>
-        <?php foreach ($newsList as $news): ?>
-            <li>
-                <a href="index.php?controller=news&action=detail&id=<?= $news['id'] ?>">
-                    <?= $news['title'] ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <?php if (!empty($news)): ?>
+        <ul>
+            <?php foreach ($news as $article): ?>
+                <li>
+                    <a href="index.php?page=news&action=detail&id=<?php echo $article['id']; ?>">
+                        <?php echo $article['title']; ?>
+                    </a>
+                    <p><strong>Chuyên mục:</strong> <?php echo $article['category_name']; ?></p>
+                    <p><strong>Ngày:</strong> <?php echo $article['created_at']; ?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>Không có tin tức nào.</p>
+    <?php endif; ?>
 </body>
 </html>
