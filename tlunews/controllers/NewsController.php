4879f37a7aa9a74d3abbcb155182391f1624bf01
Ph11 <phongphu7015@gmail.com>
2024-12-04 16:05:56 +0700

    hoan thien Them.

--- tlunews/controllers/NewsController.php
+++ tlunews/controllers/NewsController.php
@@ -11,10 +11,32 @@ class NewsController {
         $this->categoryModel = new CategoryModel($db);
     }
 
-    public function delete($id) {
-        $this->newsModel->deleteNews($id);
-        header("Location: index.php"); // Quay lại danh sách bài viết
+    public function index() {
+        $rows = $this->newsModel->getAllNews();
+        // print_r($news);
+        require('./views/home/index.php');
     }
-    
+
+    public function add() {
+        $categories = $this->categoryModel->getAllCategories();
+        require('./views/admin/news/add.php');
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $title = $_POST['title'];
+            $content = $_POST['content'];
+            $category_id = $_POST['category_id'];
+            $image = '';
+
+            if (!empty($_FILES['image']['name'])) {
+                $targetDir = "./images/";
+                $image = $targetDir . basename($_FILES['image']['name']);
+                move_uploaded_file($_FILES['image']['tmp_name'], $image);
+            }
+
+            $created_at = date('Y-m-d H:i:s');
+            $this->newsModel->addNews($title, $content, $image, $created_at, $category_id);
+            header("Location: index.php");
+        }
+    }
 }
 ?>
