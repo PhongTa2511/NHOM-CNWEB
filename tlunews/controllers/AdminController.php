566b6b58e108304403c10eb6d9095c5c8b9b5adf
PhongTa2511 <2k4phong@gmail.com>
2024-12-04 13:52:58 +0700

    Login

---
+++ tlunews/controllers/AdminController.php
@@ -0,0 +1,12 @@
+<?php
+    class AdminController{
+        private $conn;
+        public function __construct($db){
+            $this->conn = $db;
+        }
+        public function login(){}
+        public function logout() {}
+
+    }
+
+?>
